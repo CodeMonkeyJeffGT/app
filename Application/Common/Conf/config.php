e5e83dd9be12d2c567438e817df661fe813ab749
谷田 <18846938586@163.com>
2017-11-27 13:04:19 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,7 +6,7 @@ return array(
     'DB_HOST'               =>  '127.0.0.1',                // 服务器地址
     'DB_NAME'               =>  'app',                      // 数据库名
     'DB_USER'               =>  'root',                     // 用户名
-    'DB_PWD'                =>  'GT338570',                 // 密码
+    'DB_PWD'                =>  '',                 // 密码
     'DB_PORT'               =>  '3306',                     // 端口
 
     /* 日志设置 */
